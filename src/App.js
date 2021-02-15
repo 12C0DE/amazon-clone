1a5eab25239445eea096149e4c215462ea864e2f
12C0DE <rubenhnt@gmail.com>
2021-02-14 19:02:56 -0600

    home page layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Home from './Pages/Home';
+import NotFound from './Pages/NotFound';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//BEM
+	return (
+		<Router>
+			<div className="App">
+				<Switch>
+					<Route exact path="/">
+						<Header />
+						<Home />
+					</Route>
+					<Route exact path="/checkout">
+						<Header />
+						<h1>Checkout Page</h1>
+					</Route>
+					<Route path="/404">
+						<h1>404 NOT FOUND</h1>
+					</Route>
+					<Redirect to="/404" />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
